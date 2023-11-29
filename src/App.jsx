29b819aafd28a4d0f6cae8fb95c673b2263c463a
Shubhendu-12 <rajanmaurya100@gmail.com>
2023-11-29 13:40:25 +0530

    Initialised backend with express and nodemon
    Navbar color problem fixed by editing <ul> tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 const App = () => {
 
@@ -8,8 +12,12 @@ const App = () => {
 
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
      </Router>
   )
