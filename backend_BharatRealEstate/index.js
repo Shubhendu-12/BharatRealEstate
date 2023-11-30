80db1e51e209ab0ff789e8055843771d539e0b3e
Shubhendu-12 <rajanmaurya100@gmail.com>
2023-11-30 14:31:04 +0530

    Created test api route with controllers

--- backend_BharatRealEstate/index.js
+++ backend_BharatRealEstate/index.js
@@ -1,9 +1,23 @@
 // const express = require ('express');
-import  express  from "express";
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
+dotenv.config();
 const app = express();
 const port = 3000;
+const url = process.env.MONGOID;
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to MONGO ATLAS");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(port,()=>{
-    console.log(`App listening at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
+
+app.use('/api/user',userRouter)
---
+++ backend_BharatRealEstate/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type:String,
+        required:true,
+    },
+    email : {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password :{
+        type:String,
+        required:true,
+    }
+    
+},{timestamps : true});
+
+module.exports = mongoose.model("user",userSchema);
