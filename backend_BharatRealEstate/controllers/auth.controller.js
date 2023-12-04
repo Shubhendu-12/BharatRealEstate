2c2c3751801874ce5fb09d868084d205a810092c
Shubhendu-12 <rajanmaurya100@gmail.com>
2023-12-04 17:50:19 +0530

    create a middleware and a function to handle possible errors

--- backend_BharatRealEstate/controllers/auth.controller.js
+++ backend_BharatRealEstate/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 
-export const signup = async(req,res) =>{
+export const signup = async(req,res,next) =>{
 const {username,email,password} = req.body;
 const hashedPassword = bcryptjs.hashSync(password,10);
 try {
@@ -9,7 +9,8 @@ try {
 await newUser.save();
 res.status(201).json('User created sucessfully');
 } catch (error) {
-    res.status(500).json(error)
+    // res.status(500).json(error)
+    next(error);
 }
 
 
--- backend_BharatRealEstate/index.js
+++ backend_BharatRealEstate/index.js
@@ -25,3 +25,13 @@ app.listen(port, () => {
 app.use('/api/user',userRouter)
 
 app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode|| 500;
+  const message = err.message || 'Internal server error';
+  return res.status(statusCode).json({
+    sucess:false,
+    statusCode,
+    message,
+  });
+});
