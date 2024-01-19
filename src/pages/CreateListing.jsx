c4c96e35e556b513506bf25c88f5237bb1ee6eca
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-01-19 21:29:20 +0530

    complete upload images listing functionality

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const CreateListing = () => {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  console.log(formData);
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError("Image upload failed(2 mb max per image)");
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload id ${progress}`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => {
+        return i !== index;
+      }),
+    });
+  };
+  // In .filter((_,i )), The first parameter is _ or underscore which means to ignore the first parameter.
+
   return (
     <>
       <main className="mx-auto max-w-5xl p-2">
@@ -92,7 +169,6 @@ const CreateListing = () => {
                     <input
                       id="parking"
                       type="checkbox"
-                      value=""
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                     />
                     <label
@@ -108,7 +184,6 @@ const CreateListing = () => {
                     <input
                       id="furnished"
                       type="checkbox"
-                      value=""
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                     />
                     <label
@@ -124,7 +199,6 @@ const CreateListing = () => {
                     <input
                       id="offer"
                       type="checkbox"
-                      value=""
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                     />
                     <label
@@ -178,7 +252,7 @@ const CreateListing = () => {
                   aria-describedby="helper-text-explanation"
                   className="bg-gray-50 border-x-0 border-gray-300 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder=""
-                  value="3"
+                  defaultValue="3"
                   required
                 />
                 <div className="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
@@ -268,7 +342,7 @@ const CreateListing = () => {
                   aria-describedby="helper-text-explanation"
                   className="bg-gray-50 border-x-0 border-gray-300 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder=""
-                  value="3"
+                  defaultValue="3"
                   required
                 />
                 <div className="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
@@ -319,33 +393,94 @@ const CreateListing = () => {
                 Please select the number of bathrooms.
               </p>
                */}
-              
             </div>
             <div className="flex flex-col sm:flex-row  justify-between">
               {/* Regular price */}
-            <div className="">
-    <label htmlFor="regularPrice" className="block mb-2 px-2 mx-2 text-sm font-medium text-gray-900 dark:text-white">Regular Price:</label>
-    <input type="number" id="regularPrice" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$" required/>
-    </div>    
-    <div className="">
-       {/* Discounted price */}
-       <label htmlFor="discountedPrice" className="block mb-2 px-2 mx-2 text-sm font-medium text-gray-900 dark:text-white">Discounted Price:</label>
-    <input type="number" id="discountedPrice" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$" required/>
-    </div>
-
+              <div className="">
+                <label
+                  htmlFor="regularPrice"
+                  className="block mb-2 px-2 mx-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Regular Price:
+                </label>
+                <input
+                  type="number"
+                  id="regularPrice"
+                  aria-describedby="helper-text-explanation"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="$/Month"
+                  required
+                />
+              </div>
+              <div className="">
+                {/* Discounted price */}
+                <label
+                  htmlFor="discountedPrice"
+                  className="block mb-2 px-2 mx-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Discounted Price:
+                </label>
+                <input
+                  type="number"
+                  id="discountedPrice"
+                  aria-describedby="helper-text-explanation"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="$/Month"
+                  required
+                />
+              </div>
             </div>
           </div>
           <div className="flex flex-col flex-1 mx-4">
-         
-<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="images">Upload multiple images : The 1st image will be cover (Max 6)</label>
-<input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="images" type="file" accept="image/*" multiple/>
+            {/*Image Upload Section  */}
+            <label
+              className="block my-3 sm:mb-2 text-sm font-medium text-gray-900 dark:text-white "
+              htmlFor="images"
+            >
+              Upload multiple images : The 1st image will be cover (Max 6)
+            </label>
+            <input
+              onChange={(e) => {
+                setFiles(e.target.files);
+              }}
+              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              id="images"
+              type="file"
+              accept="image/*"
+              multiple
+            />
 
-<button  className="ml-2 my-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Upload</button>
+            <button
+              type="button"
+              onClick={handleImageSubmit}
+              disabled={uploading}
+              className="ml-2 my-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+              {uploading ? "Uploading" : "Upload"}
+            </button>
 
-<button  className=" ml-2 my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 uppercase">Create Listing</button>
+            <p className="text-red-600 text-sm">
+              {imageUploadError && imageUploadError}
+            </p>
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, index) => (
+                <div key={url}
+                className="flex justify-between items-center my-4">
+                  <img src={url} alt="lisitng-image" className="h-20 w-20 object-contain" />
 
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveImage(index)}
+                    className=" text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800 "
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
 
-
+            <button className=" ml-2 my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 uppercase">
+              Create Listing
+            </button>
           </div>
         </form>
       </main>
