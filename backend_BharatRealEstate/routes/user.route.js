a180efa52f9263f74b3b7541ee98f8fa60e5a9e3
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-01-22 00:47:38 +0530

    Create Get User Listings API route

--- backend_BharatRealEstate/routes/user.route.js
+++ backend_BharatRealEstate/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getUserListings, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 
@@ -11,6 +11,6 @@ router.post('/update/:id',verifyToken,updateUser);
 
 router.delete('/delete/:id',verifyToken,deleteUser);
 
-
+router.get('/listings/:id',verifyToken,getUserListings)
 
 export default router;
