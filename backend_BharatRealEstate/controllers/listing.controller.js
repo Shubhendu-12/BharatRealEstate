0b2b4e6d47020278dd2a1581ecdee91835d16c0b
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-01-23 13:03:11 +0530

    Complete Delete User Listing functionality frontend and backend

--- backend_BharatRealEstate/controllers/listing.controller.js
+++ backend_BharatRealEstate/controllers/listing.controller.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Listing from '../models/listing.model.js'
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async(req,res,next) => {
   try {
@@ -12,4 +13,20 @@ export const createListing = async(req,res,next) => {
   }
 };
 
+export const deleteListing = async (req,res,next)=>{
+  const listing = await Listing.findById(req.params.id);
 
+  if(!listing){
+    return next(errorHandler(404,'Lisitng not found'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401,'You can only update your own listings !'));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json('Listing has been deleted !');
+  } catch (error) {
+    next(error);
+  }
+};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -153,6 +153,29 @@ const Profile = () => {
      }
     }
 
+    // Delete User Listing API Route
+
+    const handleListingDelete = async (listingId)=>{
+      try {
+        
+        const res = await fetch(`http://localhost:3000/api/listing/delete/${listingId}`,{
+          method : 'DELETE',
+          credentials: "include",
+        });
+    
+        const data = await res.json();
+        if (data.success === false) {
+          console.log(data.message);
+          return;
+        }
+        setUserListings((prev)=>
+       prev.filter((listing)=>listing._id !== listingId)    
+        );
+      } catch (error) {
+        console.log(error.message)
+      }
+    };
+   
 
   return (
     <>
@@ -294,6 +317,7 @@ const Profile = () => {
                 > Edit 
                 </button>
                  <button type='button'
+                 onClick={()=>handleListingDelete(listing._id)}
                   className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 mt-2
                   uppercase"
                 > Delete
