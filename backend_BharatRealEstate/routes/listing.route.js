b08d5e31a4627cdfc1640fc1e00841f5311da4cd
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-01-23 15:01:00 +0530

    Complete Update User Listing API Route

--- backend_BharatRealEstate/routes/listing.route.js
+++ backend_BharatRealEstate/routes/listing.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 
 
 const router = express.Router();
 router.post('/create',verifyToken,createListing);
 
-router.delete('/delete/:id',verifyToken,deleteListing)
+router.delete('/delete/:id',verifyToken,deleteListing);
+
+router.post('/update/:id',verifyToken,updateListing);
+
 export default router;
