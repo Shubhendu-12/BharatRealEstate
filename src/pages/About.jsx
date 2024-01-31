6b0b3bda6cfecbc5063b2e5b48bd04fa58b24929
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-01-31 14:13:59 +0530

    Complete Contact and About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
-      about
+    <div className='max-w-7xl mx-auto flex flex-col gap-4 mt-10'> 
+    <h1 className='font-semibold text-4xl'>About Bharat  <span className='text-slate-700'>Real Estate</span></h1> 
+   <div className='max-w-5xl flex flex-col gap-5'>
+    <p>At <span className='font-semibold'> Bharat Real Estate </span> , we understand that finding the perfect property is not just about location; it's about discovering a place that truly feels like home. With a commitment to excellence and a passion for helping you find your dream property, we take pride in being your trusted partner in the real estate journey.</p>
+
+    <h2 className='text-3xl font-semibold'>
+    Why Choose Us?
+    </h2>
+
+    <ol >
+      <li className='my-2'> <span className='font-semibold '>1: Expertise:</span> Our team of seasoned real estate professionals brings a wealth of knowledge and experience to the table. From market trends to negotiation strategies, we are dedicated to guiding you through every step of the buying or selling process.
+      </li>
+      <li className='my-2'> <span className='font-semibold'>2. Personalized Service:</span>  We believe in the power of personalized service. Your goals and aspirations are unique, and we tailor our approach to ensure your real estate experience is seamless and stress-free.
+      </li>
+      <li className='my-2'>
+      <span className='font-semibold'>3. Comprehensive Listings:</span> Explore a diverse range of properties through our extensive listings. Whether you're looking for a cozy starter home, a luxury estate, or an investment opportunity, we have something for everyone.
+      </li>
+    </ol>
+
+    <Link to={'/contactUs'}  className='font-semibold hover:underline text-2xl text-blue-800' >
+      Contact Us
+    </Link>
+    <p>
+    Ready to take the next step? Contact us today to start your real estate adventure. <br/> We look forward to being your trusted partner in turning dreams into addresses.
+    </p>
+    </div>
     </div>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,114 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-      contactUs
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            Contact Us
+          </h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+             Need details about our Business investments?
+             We are here to help! You can reach us via the contact form or email.
+            Let us know.
+          </p>
+          
+          <form
+            action="https://formspree.io/f/xoqgjgnw"
+            method="POST"
+            className="space-y-8"
+          >
+            <div>
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your name
+              </label>
+              <input
+                type="name"
+                id="name"
+                name="name"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Enter your name"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="email@x.com"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Let us know how we can help you"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="6"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a message..."
+                autoComplete="off"
+                name="message"
+                required
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
+        <div className=" mx-auto">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56957.533224553954!2d80.96827814162339!3d26.844855564777394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd49fc60a843%3A0x92fc72e19b623f6a!2sGomti%20Nagar%2C%20Lucknow%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1706687024329!5m2!1sen!2sin"
+             width='100%'
+              height="450"
+              style={{ border: 0 }}
+             
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade "
+            ></iframe>
+          </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
