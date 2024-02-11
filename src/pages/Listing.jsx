cffbdc92508fc6af27e163aac3833bd1314c4f28
Shubhendu-12 <rajanmaurya100@gmail.com>
2024-02-11 13:59:59 +0530

    Final fixes in Listing Page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -130,7 +130,7 @@ const Listing = () => {
         </p> }
         {listing?.offer && <p>
         {listing.offer === true && (
-          <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">₹{' '}{+listing.regularPrice.toLocaleString('en-IN') - +listing.discountedPrice.toLocaleString('en-IN')}  discount</button>
+          <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">₹{' '}{+listing.regularPrice - +listing.discountedPrice}  discount</button>
         
         )} </p>}
         </div>
